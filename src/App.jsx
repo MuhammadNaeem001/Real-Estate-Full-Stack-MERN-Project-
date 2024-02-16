e9f76eee27992667a2f6a9afcc07599f1fdb1f7d
MuhammadNaeem001 <nk510448@gmail.com>
2024-02-16 08:16:43 -0800

    done

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-pink-800 bg-red-500 w-fit px-3 py-4 rounded-full mx-auto mt-10 cursor-pointer hover:bg-black hover:text-white animate-bounce text-3xl ">
+      Hello world!
+    </h1>
+  )
+}
