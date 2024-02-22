e6d6af2c74a923d4cb768389cbc9a873ea714ab1
MuhammadNaeem001 <nk510448@gmail.com>
2024-02-22 01:52:50 -0800

    update the header and make the profile pictuere private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import PrivateRoute from "./components/privateRoutes.jsx";
 
 
 export default function App() {
@@ -19,7 +20,9 @@ export default function App() {
     <Route path="/sign-in" element={<Signin/>}/>
     <Route path="/sign-up" element={<SignUp/>}/>
     <Route path="/about" element={<About/>}/>
+    <Route element={<PrivateRoute/>}>
     <Route path="/profile" element={<Profile/>}/>
+    </Route>
     
     
 
