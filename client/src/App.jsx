374ba4c089d4b41b02cc7e9c27768a13d706c663
MuhammadNaeem001 <nk510448@gmail.com>
2024-03-09 05:27:24 -0800

    complete listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/privateRoutes.jsx";
 import CreateListing from './pages/CreateListing.jsx';
 import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 
 export default function App() {
@@ -22,6 +23,8 @@ export default function App() {
     <Route path="/sign-in" element={<Signin/>}/>
     <Route path="/sign-up" element={<SignUp/>}/>
     <Route path="/about" element={<About/>}/>
+    <Route path="/listing/:listingId" element={<Listing/>}/>
+
     <Route element={<PrivateRoute/>}>
     <Route path="/profile" element={<Profile/>}/>
     <Route path='/create-listing' element={<CreateListing />} />
