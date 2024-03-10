1e742cf6eff3c436738fc3fdf233d70985c77e19
MuhammadNaeem001 <nk510448@gmail.com>
2024-03-10 09:30:14 -0700

    addd contact lanlord functionality in listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
@@ -136,9 +137,13 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact && (
-              <Contact listing={listing} />
+            {currentUser && listing.userRef !==currentUser._id && !contact(
+                <button onClick={()=> setContact(true)}className='bg bg-slate-700 text-white rounded-lg
+          uppercase hover:opacity-95 p-3'>Contact Landlord</button>
             )}
+
+            {contact && <Contact listing={listing}/>}
+          
           </div>
         </div>
       )}
